28d60a597d3577d2c7e367597dec8284279cf5fb
Bo-Cheng <52315454+jason71708@users.noreply.github.com>
2019-08-06 19:19:51 +0800

    Add files via upload
    fix audio

--- function.js
+++ function.js
@@ -1,16 +1,48 @@
 let vm = new Vue({
     el:'#app',
-    data:{
-        isListShow : false,
-        isPlayListShow : true,
-        isSingerShow : true,
-        isAlbumShow : false,
-        isFavoriteShow : false,
-        isplay : false,
-        volumeX : 125,
-        timeX : 0,
-        currentTime : 0,
-        maxTime : 0,
+    data() {
+        return {
+            isListShow : false,
+            isPlayListShow : true,
+            isSingerShow : true,
+            isAlbumShow : false,
+            isFavoriteShow : false,
+
+            isplay : false,
+            volumeX : 125, //進度條
+            timeX : 0,  //進度條
+            currentTime : '0:00', //顯示
+            duration : '0:00', //顯示
+            mp3Src : 'audio/遇见 Yu Jian （Encounter) - Stefanie Sun 孫燕姿 - Sungha Jung.mp3',
+            
+            singerList : ['周杰倫','盧廣仲'],
+            singerAlbum : [
+                ['魔杰座','依然范特西','12新作'],
+                ['花甲男孩轉大人','幾分之幾']
+            ],
+
+            playList : [
+                ['遇見','Photograph'],
+                ['Lucid Dreamer','Sunspots'],
+            ],
+            singerSongs : [
+                ['給我一首歌的時間','花海','說好的幸福呢','稻香','夜的第七章','听妈妈的话','千里之外','紅塵客棧','明明就','比較大的大提琴'],
+                ['明仔載-演奏版','繁星五號','快魚仔','幾分之幾','你是我的水']
+            ],
+            AlbumSongs :[
+                ['給我一首歌的時間','花海','說好的幸福呢','稻香'],
+                ['夜的第七章','听妈妈的话','千里之外'],
+                ['紅塵客棧','明明就','比較大的大提琴'],
+                ['明仔載-演奏版','繁星五號','快魚仔'],
+                ['幾分之幾','你是我的水']
+            ],
+            myFavorite : ['只能想念你','思念是一種病','稻香','愛情怎麼了嗎'],
+            
+            bigTitle : '工作歌單' ,
+            albumImgSrc : 'img/maxresdefault (2).jpg',
+            songTitle : '遇見',
+            singer : 'Sungha Jung'
+        }
     },
     computed:{
         outPutIcon(){
@@ -33,7 +65,10 @@ let vm = new Vue({
             let xxx = audio.currentTime / audio.duration * 552
             this.timeX = xxx
         },
-        
+        test(){
+            let audio =document.querySelector('#audio');
+            return this.minSec(audio.duration)
+        },
     },
     methods:{
         listShow(){
@@ -70,9 +105,9 @@ let vm = new Vue({
                 }else{
                     this.timeX = Xer + or
                 }
+                audio.currentTime = this.timeX / 552 * audio.duration;
             };
             document.onmouseup = ()=> {
-                audio.currentTime = this.timeX / 552 * audio.duration;
                 document.onmousemove=null;
                 document.onmousedown=null;
             };
@@ -89,12 +124,21 @@ let vm = new Vue({
         },
         minSec(time){
             let min = Math.floor(time/60);
-            let sec = Math.round(time%60);
-            return min+':'+sec;
+            let sec = '0' + Math.round(time%60);
+            let sec2 = sec.slice(-2)
+            return min+':'+sec2;
         },
-        onTimeUpdate () {
-            console.log(this)
-            this.currentTime = this.$refs.mp3.currentTime
+        test3(){
+            let audio =document.querySelector('#audio');
+            console.log(audio.currentTime)
+        },
+        timeUpdate(){
+            let timeStr = parseInt(this.$refs.mp3.currentTime);
+            this.currentTime = this.minSec(timeStr);
+            this.timeX = timeStr / this.$refs.mp3.duration * 552
+        },
+        canPlay(){
+            this.duration = this.minSec(this.$refs.mp3.duration);
         }
     }
 })
